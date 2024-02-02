bffa45d700a1f7e8904df98cf1478217dfaf18d9
Ezz Eldien Ayman <ezzeldien490@gmail.com>
2024-02-02 06:53:19 -0800

    updated tsak2

--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -4,3 +4,6 @@ def replace_in_list(my_list, idx, element):
     if idx == 0 or idx in range(len(my_list)):
         my_list[idx] = element
         return (my_list)
+
+    else:
+        return (my_list)
