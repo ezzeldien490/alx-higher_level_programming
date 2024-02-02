271639f3649d65a24c66f2befffef24d64250fd9
Ezz Eldien Ayman <ezzeldien490@gmail.com>
2024-02-02 05:40:21 -0800

    updated

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 
 def element_at(my_list, idx):
-    if idx in range(len(my_list)):
+    if idx in range(len(my_list) - 1):
         return(my_list[idx])
     else:
         return(none)
