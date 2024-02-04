efc9ace9bf903e046ecc1ab0db1355b88513defb
Ezz Eldien Ayman <ezzeldien490@gmail.com>
2024-02-04 09:34:38 -0800

    task100

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -5,7 +5,7 @@ if __name__ == "__main__":
     from calculator_1 import add, sub, div, mul
 
 n = len(argv)
-if n < 3:
+if n < 4:
     print("Usage: ./100-my_calculator.py <a> <operator> <b>")
     exit(1)
 
