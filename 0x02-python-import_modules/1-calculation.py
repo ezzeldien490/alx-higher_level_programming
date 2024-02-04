c83188a5a6d87ffeca6044e7a5a7a9993ec06c34
Ezz Eldien Ayman <ezzeldien490@gmail.com>
2024-02-04 04:25:41 -0800

    l

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -6,7 +6,7 @@ if __name__ == "__main__":
 a = 10
 b = 5
 
-print("{} + {} = {}".format(a, b, add(10, 5)))
-print("{} - {} = {}".format(a, b, sub(10, 5)))
-print("{} * {} = {}".format(a, b, mul(10, 5)))
-print("{} / {} = {}".format(a, b, div(10, 5)))
+print("{} + {} = {}".format(a, b, add(a, b)))
+print("{} - {} = {}".format(a, b, sub(a, b)))
+print("{} * {} = {}".format(a, b, mul(a, b)))
+print("{} / {} = {}".format(a, b, div(a, b)))
