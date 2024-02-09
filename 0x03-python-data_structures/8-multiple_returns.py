e2066af99c81158270e61a36642b31de4570ef27
Ezz Eldien Ayman <ezzeldien490@gmail.com>
2024-02-09 06:44:54 -0800

    task8

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,5 +1,9 @@
 #!/usr/bin/python3
 
 def multiple_returns(sentence):
-    tuple = (len(sentence), sentence[0])
+    if len(sentence) == 0:
+        tuple = (len(sentence), None)
+    else:
+        tuple = (len(sentence), sentence[0])
+
     return (tuple)
